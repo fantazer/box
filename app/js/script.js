e2491b37467c13795e131309662e5c2211596075
Your Name <you@example.com>
2016-12-07 12:16:29 +0300

    first commit

---
+++ app/js/script.js
@@ -0,0 +1,126 @@
+$(document).ready(function(){
+	/* ###### For only ies  ######*/
+	//if(/MSIE \d|Trident.*rv:/.test(navigator.userAgent)){
+	//	//code
+	//}
+
+	//message for old IE
+	function isIE () {
+	  var myNav = navigator.userAgent.toLowerCase();
+	  return (myNav.indexOf('msie') != -1) ? parseInt(myNav.split('msie')[1]) : false;
+	}
+
+	if (isIE() < 10 &&  isIE()) {
+		$('body').empty();
+		$('body').prepend('<div class="old-browser"><div class="old-browser-text"> Браузер не поддерживается =(</div></div>');
+	}
+
+	//for init SVG 
+	svg4everybody();
+
+	$(".slider__wraper").owlCarousel({
+	 items : 1,
+	 autoHeight : true,
+	 dots: true,
+	 autoplay : true,
+	 singleItem:true,
+	 nav:false
+	 }
+	);
+	/* ###### For SlideToggle Elements  ######*/
+	var hideToggle = function(targetClick,toggleEl) {
+		$(targetClick).click(function(event){
+				event.stopPropagation();
+				$(toggleEl).slideToggle("fast");
+		});
+		$(toggleEl).on("click", function (event) {
+			event.stopPropagation();
+		});
+		$(document).on("click", function () {
+				$(toggleEl).hide();
+		});
+	}
+	hideToggle('.nav-toggle','.nav-menu  ');
+
+
+	/* ###### init RangeSLider  ######*/
+	/* ###### bower i --save-dev nouislider  ######*/
+	/* ###### https://gist.github.com/fantazer/2bdc4e6a63708e143718ffa7c32eae17  ######*/
+
+	/*var slider = document.getElementById('rangeSlider'); //Элемент
+
+	noUiSlider.create(slider, {
+		start: [0, 100],
+		connect: true,
+		step: 10,
+		range: {
+			'min': 0,
+			'max': 100,
+		},
+		pips: { // Show a scale with the slider
+			mode: 'steps',
+			density: 4
+		}
+	});*/
+	
+
+	
+})
+
+//cash SVG
+
+;( function( window, document )
+{
+	'use strict';
+
+	var file  = 'img/sprite.html',
+		revision = 1;
+
+	if( !document.createElementNS || !document.createElementNS( 'http://www.w3.org/2000/svg', 'svg' ).createSVGRect )
+		return true;
+
+	var isLocalStorage = 'localStorage' in window && window[ 'localStorage' ] !== null,
+		request,
+		data,
+		insertIT = function()
+		{
+			document.body.insertAdjacentHTML( 'afterbegin', data );
+		},
+		insert = function()
+		{
+			if( document.body ) insertIT();
+			else document.addEventListener( 'DOMContentLoaded', insertIT );
+		};
+
+	if( isLocalStorage && localStorage.getItem( 'inlineSVGrev' ) == revision )
+	{
+		data = localStorage.getItem( 'inlineSVGdata' );
+		if( data )
+		{
+			insert();
+			return true;
+		}
+	}
+
+	try
+	{
+		request = new XMLHttpRequest();
+		request.open( 'GET', file, true );
+		request.onload = function()
+		{
+			if( request.status >= 200 && request.status < 400 )
+			{
+				data = request.responseText;
+				insert();
+				if( isLocalStorage )
+				{
+					localStorage.setItem( 'inlineSVGdata',  data );
+					localStorage.setItem( 'inlineSVGrev',   revision );
+				}
+			}
+		}
+		request.send();
+	}
+	catch( e ){}
+
+}( window, document ) );
